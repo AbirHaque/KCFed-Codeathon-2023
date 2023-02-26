0f373d629b3e8732f1d3334065db55669f099be4
shaddev <shadahmed2015@gmail.com>
2023-02-25 20:27:13 -0600

    update tutorials

--- home-automation-project/src/tutorials.tsx
+++ home-automation-project/src/tutorials.tsx
@@ -2,6 +2,8 @@ import { TutorialType } from "./types"
 import allLightsOn from './assets/allLightsOn.png'
 import allLightsOff from './assets/allLightsOff.png'
 import oneLightOn from './assets/oneLightOn.png'
+import alternateLights from './assets/alternateLights.gif'
+import specificTimeLightOn from './assets/specificTimeLightOn.png'
 
 export const allTutorials: TutorialType[] = [
     {tutorialTitle: 'Variables',
@@ -9,11 +11,35 @@ export const allTutorials: TutorialType[] = [
     resultImage: oneLightOn,
     codePanel: {str_items: '<div id="l1">light1=get_light() # obtain the light bulb</div>\n<div id="l2">light1=<input id="q1" placeholder="Boolean"></input> # switch the lightbulb on</div>',
                 answers: 'True'},
-    summaryDescription: 'Welcome to the first tutorial in learning and applying Python programming to home automation! Over the course of these five tutorials, you will not only master the fundamentals coding, but also learn how to turn your home smart. In our first lesson, we will explore how we may switch a light bulb in our home on using the power of code. We will learn about variables and their various types and leave you to simulate the light bulb and watch the light fill the rooms using Python code.',
+    summaryDescription: 'Welcome to the first tutorial in learning and applying Python programming to home automation! Over the course of these five tutorials, you will not only master the fundamentals coding, but also learn how to turn your home smart. In our first lesson, we will explore how we may switch a single light bulb in our home on using the power of code. We will learn about variables and their various types and leave you to simulate the light bulb and watch the light fill the living room using Python code.',
     tutorialDescription: `When coding any program, we need to define what kinds of objects, whether they be numbers, strings (sequences of characters), or in our case, home appliances, we are working with. We do this using variables. To assign a variable, we use the syntax:
+
 \`\`\`variable_name=value\`\`\`
-The values True and False here are also called **boolean** values, which is a fancy way of saying they represent the 'truthiness' of something. We will see them appear again in the next lesson when we code up conditionals, in our case, the state of the light bulb. In the editing space to the right, we want to switch the light bulb on. Remember, a variable must always be defined before it can be modified!`},
-,
+
+The values True and False here are also called **boolean** values, which is a fancy way of saying they represent the 'truthiness' of something, in our case, the state of the light bulb. Here is another examples of assigning and using variables in conjunction with one another:
+
+\`\`\`hello = "Hello "
+world = "World!"
+message = hello + world
+print(message)
+\`\`\`
+
+We use the built-in function (more on that later!) print to print out a string. In the editing space to the right, we want to switch the light bulb on. Remember, a variable must always be defined before it can be modified!`},
+
+{tutorialTitle: 'Conditional statements',
+    baseImage: alternateLights,
+    resultImage: specificTimeLightOn,
+    codePanel: {str_items: '<div id="l1">bulb=False #Initialize bulb variable to off</div>\n<div id="l2">cur_time=17 #Set time to 5:00PM</div>\n<div id="l3">if cur_time>=<input id="q1" placeholder="Integer"></input>:</div>\n<div id="l4">\t bulb= <input id="q2" placeholder="Boolean"</input></div>',
+                answers: '17\nTrue'},
+    summaryDescription: 'You will learn about how to use conditional statements in real life applications.',
+    tutorialDescription: `Conditional statements, also known as control structures, are used in programming to enable the computer to make decisions based on certain conditions. Conditional statements allow programmers to control the flow of execution of their code based on the values of variables or other data.
+\`\`\`
+if statement:
+  condition to be executed 
+\`\`\`
+The purpose of this code is to simulate the behavior of light bulb. The cur_time variable represents the current time of day.The if statement tells the bulb to turn on if the current time is 5pm or later.
+ `
+},
 {tutorialTitle: 'For Loops and Lists',
     baseImage: allLightsOff,
     resultImage: allLightsOn,
